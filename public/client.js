cecf513a36a2497b7922afc71773d1d891d75d2a
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2021-02-04 20:17:42 -0300

    client address changed and meta description added

--- public/client.js
+++ public/client.js
@@ -1,21 +1,21 @@
-const socket = io("http://10.0.0.117:3000");
+const socket = io('http://localhost:3000');
 
 // login
-const login = document.querySelector("#login");
+const login = document.querySelector('#login');
 
-login.addEventListener("submit", event => {
+login.addEventListener('submit', event => {
     event.preventDefault();
 
     const nickname = event.target.nickname.value;
 
     socket.nickname = nickname;
     
-    login.classList.add("hide");
+    login.classList.add('hide');
 });
 
-const chat = document.getElementById("chat");
+const chat = document.getElementById('chat');
 
-chat.addEventListener("submit", event => {
+chat.addEventListener('submit', event => {
     event.preventDefault();
     
     const message = event.target.message.value;
@@ -23,25 +23,25 @@ chat.addEventListener("submit", event => {
 });
 
 const sendMessage = (nickname, message) => {
-    if (message !== "")  {
-        const data = { id: socket.id, nickname, message, messageSide: "right" };
-        socket.emit("newMessage", data);
+    if (message !== '')  {
+        const data = { id: socket.id, nickname, message, messageSide: 'right' };
+        socket.emit('newMessage', data);
         renderMessage(data);
-        event.target.message.value = "";
+        event.target.message.value = '';
     }
-}
-
+};
+  
 const newElement = (tagName, className) => {
     const element = document.createElement(tagName);
     element.className = className;
     
     return element;
-}
+};
 
-const createMessage = (data) => {
-    const message = newElement("div", "message");
-    const nickname = newElement("div", "nickname");
-    const messageBody = newElement("div", "message-body");
+const createMessage = data => {
+    const message = newElement('div', 'message');
+    const nickname = newElement('div', 'nickname');
+    const messageBody = newElement('div', 'message-body');
 
     message.classList.add(data.messageSide);
     
@@ -52,31 +52,29 @@ const createMessage = (data) => {
     message.appendChild(messageBody);
     
     return message;
-}
+};
 
 const renderMessage = data => {
-
     if (data.id !== socket.id) {
-        data.messageSide = "left"
+        data.messageSide = 'left'
     }
-    const messageLog = document.querySelector(".message-log");
+  
+    const messageLog = document.querySelector('.message-log');
     const message = createMessage(data);
     messageLog.appendChild(message);
 
     scrollToBottom(messageLog);
-}
-
+};
 
 const scrollToBottom = element => {
     element.scrollTo(0, element.scrollHeight);
-}
-
+};
 
 // socket events
-socket.on("previousMessages", messageLog => {
+socket.on('previousMessages', messageLog => {
     messageLog.forEach(message => renderMessage(message));
 });
 
-socket.on("newMessage", data => {
+socket.on('newMessage', data => {
     renderMessage(data);
 });
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const port = 3000;
 
 server.listen(port, () => {
     console.log(`running on http://localhost:${port}`);
 });
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (request, response) => {
-    response.render(__dirname + "/public/index.html");
+app.get('/', (request, response) => {
+    response.render(__dirname + '/public/index.html');
 });
 
 let messageLog = [];
 let connecteds = 0;
 
-io.on("connection", socket => {
+io.on('connection', socket => {
     console.log(`${++connecteds} connected`);
 
-    socket.emit("previousMessages", messageLog);
+    socket.emit('previousMessages', messageLog);
 
-    socket.on("newMessage", data => {
+    socket.on('newMessage', data => {
         messageLog.push(data);
-        socket.broadcast.emit("newMessage", data);
+        socket.broadcast.emit('newMessage', data);
     });
 
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
         console.log(`${--connecteds} connected`);
     });
 });
